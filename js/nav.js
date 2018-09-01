f726b9518efc5df85cb7a18ef8487f40c40c7b7e
Xlind <719852970@qq.com>
2018-09-02 01:43:15 +0800

    搞定侧导航，开始中间部分

--- js/nav.js
+++ js/nav.js
@@ -1,25 +1,24 @@
 $(function(){
-
-    // nav收缩展开
-    $('.navLive-item>a').on('click',function(){
-        if (!$('.navLive').hasClass('navLive-mini')) {
-            if ($(this).next().css('display') == "none") {
-                //展开未展开
-                $('.navLive-item').children('ul').slideUp(300);
-                $(this).next('ul').slideDown(300);
-                $(this).parent('li').addClass('navLive-show').siblings('li').removeClass('navLive-show');
-            }else{
-                //收缩已展开
-                $(this).next('ul').slideUp(300);
-                $('.navLive-item.navLive-show').removeClass('navLive-show');
-            }
-        }
-    });
+    // // nav收缩展开
+    // $('.navLive-item>a').on('click',function(){
+    //     if (!$('.navLive').hasClass('navLive-mini')) {
+    //         if ($(this).next().css('display') == "none") {
+    //             //展开未展开
+    //             $('.navLive-item').children('ul').slideUp(300);
+    //             $(this).next('ul').slideDown(300);
+    //             $(this).parent('li').addClass('nav-show').siblings('li').removeClass('nav-show');
+    //         }else{
+    //             //收缩已展开
+    //             $(this).next('ul').slideUp(300);
+    //             $('.navLive-item.navLive-show').removeClass('nav-show');
+    //         }
+    //     }
+    // });
 
     //nav-mini切换
     $('#mini').on('click',function(){
         if (!$('.navLive').hasClass('navLive-mini')) {
-            $('.navLive-item.nav-show').removeClass('navLive-show');
+            // $('.navLive-item.nav-show').removeClass('nav-show');
             $('.navLive-item').children('ul').removeAttr('style');
             $('.navLive').addClass('navLive-mini');
         }else{
